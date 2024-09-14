d1b48d4fb56520b6e1828394f228615a2e3794b0
HSS <151983436+HarshithaShashi@users.noreply.github.com>
2024-09-14 19:59:00 +0530

    final

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,14 +1,61 @@
-import React from 'react';
-import './CountDown.css';
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import "./CountDown.css";
 
-const CountDown =() =>{
-    return(
-        <section className="hero">
-        <div className="overlay">
-          <h2>We are going to...</h2>
-          <h1>Celebrate Our Love</h1>
-        </div>
-      </section>
+function TimeSection() {
+  const targetDate = useMemo(() => new Date("2024-10-13T23:59:00"), []);
+
+  const calculateTimeRemaining = useCallback(() => {
+    const now = new Date();
+    const timeDifference = targetDate - now;
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
-};
-export default CountDown;
+    const minutes = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    return { days, hours, minutes, seconds };
+  }, [targetDate]);
+  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining);
+  // console.log(timeRemaining)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining(calculateTimeRemaining());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [calculateTimeRemaining]);
+
+
+  return (
+    <div className="Count" id="timeSection">
+      <div className="Count-Heading">
+        <span></span>
+        We are waiting for.....
+        <br />
+        <span className="Count-Heading-Main">The adventure</span>
+      </div>
+      <div className="clock">
+        <div className="countdown">
+          <div>{timeRemaining.days}</div>
+          <div className="count-days">Days</div>
+        </div>
+        <div className="countdown">
+          <div>{timeRemaining.hours}</div>
+          <div className="count-days">Hours</div>
+        </div>
+        <div className="countdown">
+          <div>{timeRemaining.minutes}</div>
+          <div className="count-days">Minutes</div>
+        </div>
+        <div className="countdown">
+          <div>{timeRemaining.seconds}</div>
+          <div className="count-days">Seconds</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TimeSection;
